6348b36c6457c4027b5e323583489cda0612fda6
Mohit Patni <mohitpatni0731@gmail.com>
2025-07-10 15:38:55 +0530

    bye

--- src/pages/AIMagic.tsx
+++ src/pages/AIMagic.tsx
@@ -21,7 +21,7 @@ const customStyles = `
   @keyframes gradient-x {
     0%, 100% { background-position: 0% 50%; }
     50% { background-position: 100% 50%; }
-  } 
+  }  
   
   @keyframes fade-in-up {
     from {
